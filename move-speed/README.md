a356862c03dbee75148831187c12a278044cca4e
Blaine Price <1wbprice@gmail.com>
2017-12-03 23:39:56 -0500

    Write partial solution for problem 2 in javascript

---
+++ move-speed/README.md
@@ -0,0 +1,12 @@
+# All Paths
+
+## Task
+Given that 
+- a tank exists on a Cartesian grid at [0, 0] 
+- a moving on the x-axis and y-axis is half as expensive
+
+List all the paths that it could take using 2 steps 
+
+## Rules
+- The tank can only move twice
+- The tank's path cannot loop back on itself (it can't return to where it started)
---
+++ move-speed/solution.js
@@ -0,0 +1,159 @@
+'use strict';
+
+/**
+ * Describes a single point on a Cartesian grid
+ * @typedef {Object} Point
+ * @property {Number} x - The x position of a point.
+ * @property {Number} y - The y position of a point.
+ */
+
+/**
+ * An array of points that describes the path a unit might
+ * take from start to finish.
+ * @typedef {Array[Point]} Path
+ */
+
+/**
+ * Given an x and y coordinate, returns a Point
+ * @param {Number} x
+ * @param {Number} y
+ * @returns {Point}
+ */
+
+function getPoint(x, y) {
+    return {
+        x,
+        y
+    }
+}
+
+/**
+ * Creates a new Point one position above point
+ * @param {Point} point
+ * @returns {Point}
+ */
+
+function moveUp(point) {
+    return {
+        x: point.x,
+        y: point.y + 1
+    }
+}
+
+/**
+ * Creates a new Point one position below point
+ * @param {Point} point
+ * @returns {Point}
+ */
+
+function moveDown(point) {
+    return {
+        x: point.x,
+        y: point.y - 1
+    }
+}
+
+/**
+ * Creates a new Point one position to the left of point
+ * @param {Point} point
+ * @returns {Point}
+ */
+
+function moveLeft(point) {
+    return {
+        x: point.x - 1,
+        y: point.y
+    }
+}
+
+/**
+ * Creates a new Point one position to the right of point
+ * @param {Point} point
+ * @returns {Point}
+ */
+
+function moveRight(point) {
+    return {
+        x: point.x + 1,
+        y: point.y
+    }
+}
+
+function getMoveCost(point) {
+    if ((point.x >= -2 && point.x <= 2) && point.y === 0) {
+        return .5
+    }
+    return 1 
+}
+
+function getPathStart(point, speed) {
+    return [Object.assign({}, point, {
+        speed
+    })]
+}
+
+function last(array) {
+    return array.slice(-1)[0]
+}
+
+function findNextMoves(path) {
+    const lastPoint = last(path)
+    return [moveUp, moveRight, moveDown, moveLeft]
+    .map(action => action(lastPoint))
+    .filter(move => !pathLoopsback(path, move))
+    .filter(move => lastPoint.speed >= getMoveCost(move))
+}
+
+function pathLoopsback(path, move) {
+    return path.find(muv => {
+        return muv.x === move.x && muv.y === move.y
+    })
+}
+
+function expandPath(path, nextMoves) {
+    const lastMove = last(path)
+
+    if (!nextMoves) {
+        nextMoves = findNextMoves(path)
+    }
+
+    return nextMoves.map(move => {
+        return [
+            ...path,
+            Object.assign({}, move, {
+                speed: lastMove.speed - getMoveCost(move)
+            })
+        ]
+    })
+}
+
+function expandPaths(paths) {
+    return paths.reduce((memo, path) => {
+        const nextMoves = findNextMoves(path)
+        if (nextMoves.length > 0) {
+            return [
+                ...memo,
+                ...expandPath(path, nextMoves)
+            ]
+        } else {
+            memo.push(path)
+        }
+        return memo
+    }, [])
+}
+
+function getPaths(input, speed) {
+    if (!input.length) {
+        return getPaths(expandPath(getPathStart(input, speed)))
+    }
+
+    const results = expandPaths(input)
+    // recursive case
+    if (results.length > input.length) {
+        return getPaths(results)
+    }
+    // base case
+    return results
+}
+
+console.log(getPaths(getPoint(0, 0), 2))
